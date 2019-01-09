f355d701227d78099d238e30dca326192fc96813
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-09 20:19:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13902,44 +13902,5 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    
    }
    }); 
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("اي دي سيرفرك - Server id");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
-
-
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("531930430035984384");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("490856254756093985");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
 
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DxUvbA.t7-eq0Ot-LSR5SlwrzXtG3mXWj8');
