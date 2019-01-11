8738ab90fcaa91f51165c2e6dcdb4ab819bcc8c4
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-11 16:11:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13940,45 +13940,40 @@ client.on("message", (message) => {
 })
 
 const devs = ["475233499641806849"]
- 
-const adminprefix = "!";
+
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
      
-  if (message.content.startsWith(adminprefix + 'pt')) {
+  if (message.content.startsWith(prefix + 'pt')) {
     client.user.setGame(argresult);
       message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
   } else
-    if (message.content === (adminprefix + "Percie")) {
+    if (message.content === (prefix + "Percie")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
+  if (message.content.startsWith(prefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'setprefix')) {
-  client.user.setPrefix(argresult).then
-      message.channel.send(`**Prefix Changed :white_check_mark: ${argresult}** `)
-  } else
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(prefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
   } else     //Narox
-    if (message.content.startsWith(adminprefix + 'setname')) {
+    if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`**${argresult}** : Done `)
   return message.reply("**Name Changed :white_check_mark:**");
   } else
-    if (message.content.startsWith(adminprefix + 'setavatar')) {
+    if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
         } else    
-  if (message.content.startsWith(adminprefix + 'st')) {
+  if (message.content.startsWith(prefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
   }
-    if(message.content === adminprefix + "restart") {
+    if(message.content === prefix + "restart") {
       if (!devs.includes(message.author.id)) return;
           message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
         console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -13990,5 +13985,6 @@ client.on('message', message => {
     }
  
   });
+
 
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DxUvbA.t7-eq0Ot-LSR5SlwrzXtG3mXWj8');
