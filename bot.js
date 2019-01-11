1f71eaf52a729f679450ba8e4d7f3a53fa1cea7d
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-11 22:29:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13943,7 +13943,7 @@ client.on("message", (message) => {
 client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('*bcallserver')){
+if (message.content.startsWith('!bcall')){
 if(!message.author.id === '475233499641806849') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
