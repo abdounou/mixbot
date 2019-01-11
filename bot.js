0f1383b9e8e1c90bd11502505985b3ebfa2a5351
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-11 21:33:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13940,4 +13940,16 @@ client.on("message", (message) => {
     }
 })
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('*bcallserver')){
+if(!message.author.id === 'ايدي حسابك مو حساب بوت') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DxqMfA.PyaiGAzn4ulux1AUXITnBwU-44w');
