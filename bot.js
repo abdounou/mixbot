7c9162742d28272788616dae1a1ee4184f5137fa
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-11 17:11:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1159,6 +1159,24 @@ client.on('ready',  () => {
                         }
                     });
                     
+client.on('ready', function(){
+    client.user.setStatus("dnd");
+    var ms = 100000 ;
+    var setGame = [`!help Servers ${client.guilds.size} `,`!invite Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
+
+});
 
 //كود الاوان//
 
