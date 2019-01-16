64187249106dde89f74c39b6c93d8f1981d1bcc1
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-16 23:09:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14096,7 +14096,7 @@ client.on('guildMemberAdd', member => {
         .setAuthor(m.username,m.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
      
-         .setFooter(`${m.tag}`,"https://cdn.discordapp.com/attachments/528961037530169377/533620251527610378/d4b3f823c22caf76.jpg")
+         .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:yumz});          
          
    
@@ -14160,6 +14160,7 @@ welcomer.sendFile(canvas.toBuffer())
       });                    
  }
 });
+
 
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
