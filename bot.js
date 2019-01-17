802030cc8ab0b9abf58171d8e2f3bd088a682737
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-17 22:09:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14143,14 +14143,5 @@ welcomer.sendFile(canvas.toBuffer())
  }
 });
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const channel = member.guild.channels.find("name", "welcome");
-     channel.send(`<@${member.user.id}> ** joined; ** Invited by ** <@${inviter.id}> ** `);
-  });
-});
 
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DxzXFQ.riM6mlDUlFkS6Mo9K8VvNQLDzxA');
