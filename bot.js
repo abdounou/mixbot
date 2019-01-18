cc2ce2083b8ab6ebd4d690d3b2cb634e282b9d14
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-18 19:59:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14063,9 +14063,13 @@ if(message.content.startsWith("!restart")){
 });
 
 
-client.on('guildMemberAdd', member => {
 
-     const welcomer =  member.guild.channels.find('name', 'chat');
+const fs = require('fs');
+const jimp = require('jimp');
+const Canvas = require('canvas');
+ 
+client.on('guildMemberAdd', member => {
+     const welcomer =  member.guild.channels.find('name', 'welcome');
     if(!welcomer) return;
       if(welcomer) {
          moment.locale('ar-ly');
@@ -14075,16 +14079,17 @@ client.on('guildMemberAdd', member => {
         .setThumbnail(m.avatarURL)
         .setAuthor(m.username,m.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-      
+     
          .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:yumz});          
          
-    
-
-
-
+   
+ 
+ 
+ 
 const w = ['./img/w1.png'];
-
+ 
+         let Image = Canvas.Image,
             canvas = new Canvas(400, 200),
             ctx = canvas.getContext('2d');
         fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
@@ -14094,20 +14099,20 @@ const w = ['./img/w1.png'];
             ground.src = Background;
             ctx.drawImage(ground, 0, 0, 400, 200);
              
-          
-
+         
+ 
                 let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
                 jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
-                        
+                       
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
                         ctx.fillText(`welcome to ${member.guild.name}`, 300, 130);
-                        
+                       
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
@@ -14122,19 +14127,19 @@ const w = ['./img/w1.png'];
                               ctx.stroke();
                                  ctx.clip();
                                  ctx.drawImage(ava, 13, 38, 128, 126);  
-                          
-                
+                         
+               
                              
 welcomer.sendFile(canvas.toBuffer())
-
-
-
-      
-      
+ 
+ 
+ 
+     
+     
                     }  )  
-      
-                    
-
+     
+                   
+ 
 })
       });                    
  }
