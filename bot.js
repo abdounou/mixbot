d8a8fafe7bd8efbe6cef490c678fc86aac9c8d40
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-18 18:48:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14197,7 +14197,6 @@ hero.on('voiceStateUpdate', (u, member) => {
   setInterval(() => {
     if(!member.voiceChannel) return;
     if(member.selfDeafen) return;
-    vpoints[author].points += rPoints;
   }, 5000); // 5 Secs
 });
 
