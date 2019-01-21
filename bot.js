4cfc10980f38867e3e3c08ae84d64803fbcc508b
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-21 18:51:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13981,15 +13981,6 @@ function play(guild, song) {
 }
 });
 
-const channel = sWlc[message.guild.id].channel
-  if (message.content.startsWith(prefix + "setwelcomer")) {
-    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
-    let newChannel = message.content.split(' ').slice(1).join(" ")
-    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
-    sWlc[message.guild.id].channel = newChannel
-    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
-  }
-});
 
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
