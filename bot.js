397e28f080e2538113db6e445aadc393fda51eaf
abdounou <45997213+abdounou@users.noreply.github.com>
2019-01-21 20:38:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14078,5 +14078,62 @@ client.on("guildMemberAdd", member => {
 
     })
 
+const hero = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
+const as = require('array-sort');
+const config = { prefix: "!", token: "NTI5NjA5NTM1NTQ4MTYyMDU5.DyOrwA.fvbdO_o3Xl2a-7R-0IDvqA0Joek" };
+const tpoints = {};
+const vpoints = {};
+hero.config = config;
+hero.login(hero.config.token);
+hero.on('ready',async () => {
+  hero.users.forEach(m => {
+    if(m.bot) return;
+    if(!tpoints[m.id]) tpoints[m.id] = {points: 0, id: m.id};
+ 
+    if(!vpoints[m.id]) vpoints[m.id] = {points: 0, id: m.id};
+  });
+});
+ 
+hero.on('message',async message => {
+  if(message.author.bot || message.channel.type === 'dm') return;
+  let args = message.content.split(' ');
+  let member = message.member;
+  let mention = message.mentions.users.first();
+  let guild = message.guild;
+  let author = message.author;
+ 
+  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
+  tpoints[author.id].points += rPoints;
+  if(args[0] === `${hero.config.prefix}top`) {
+    let _voicePointer = 1;
+    let _textPointer = 1;
+    let _voiceArray = Object.values(vpoints);
+    let _textArray = Object.values(tpoints);
+    let _topText = as(_textArray, 'points', { reverse: true });
+    let _topVoice = as(_voiceArray, 'points', { reverse: true });;
+    let topRoyale = new Discord.RichEmbed();
+    topRoyale.setAuthor(message.author.username, message.author.avatarURL);
+    topRoyale.setTitle('# " Top');
+    //topRoyale.setThumbnail(message.guild.iconURL);
+    topRoyale.addField(`**TOP 5 TEXT 💬**`, _topText.map(r => `**\`.${_textPointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).slice(0, 5), true);
+    topRoyale.addField(`**TOP 5 VOICE 🎙**`, _topVoice.map(r => `**\`.${_voicePointer++}\` | <@${r.id}> \`XP: ${r.points}\`**`).slice(0, 5), true);
+    topRoyale.setFooter(`Developed By: YouseeF.#9060`, message.guild.iconURL);
+    message.channel.send(topRoyale).catch(e => {
+      if(e) return message.channel.send(`**. Error; \`${e.message}\`**`);
+    });
+  }
+});
+ 
+hero.on('voiceStateUpdate', (u, member) => {
+  let author = member.user.id;
+  let guild = member.guild;
+  if(member.voiceChannel === null) return;
+  let rPoints = Math.floor(Math.random() * 4) + 1;// Random Points
+  setInterval(() => {
+    if(!member.voiceChannel) return;
+    if(member.selfDeafen) return;
+    vpoints[author].points += rPoints;
+  }, 5000); // 5 Secs
+});
 
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DyOrwA.fvbdO_o3Xl2a-7R-0IDvqA0Joek');
