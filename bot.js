eccdeb6e401c90b6646809e3a3cb1925dcf0b2b1
abdounou <45997213+abdounou@users.noreply.github.com>
2019-02-16 14:02:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14111,4 +14111,38 @@ hero.on('message',async message => {
   }
 });
 
+client.on('message', async msg => { //Toxic Codes
+    if (msg.content.toLowerCase() === "prefix" + "disco") {//Toxic Codes
+   if (msg.channel.type === "dm") return; 
+  const renk = [//Toxic Codes
+  'DEFAULT',
+  'BLACK',
+  'GREEN',
+  'BLUE',
+  'PURPLE',
+  'GOLD',
+  'ORANGE',
+  'RED',
+  'GREY',
+  'DARKER_GREY',
+  'NAVY',
+  'DARK_AQUA',
+  'DARK_GREEN',
+  'DARK_BLUE',
+  'DARK_PURPLE',
+  'DARK_GOLD',
+  'DARK_ORANGE',
+  'DARK_RED',
+  'DARK_GREY',
+  'LIGHT_GREY',
+  'DARK_NAVY',
+  'BEİGE'
+];
+  setInterval(function() {//Toxic Codes
+        var random = Math.floor(Math.random()*(renk.length-0+1)+0);//Toxic Codes
+      msg.guild.roles.find('name', "Disco").setColor(renk[random])//Toxic Codes
+      }, 4000);//سرعه تغير الاوان
+  }//Toxic Codes
+});
+
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DyOrwA.fvbdO_o3Xl2a-7R-0IDvqA0Joek');
