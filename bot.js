72df7342268db68a135de8b9739f59734aa58e18
abdounou <45997213+abdounou@users.noreply.github.com>
2019-03-08 19:02:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14276,4 +14276,44 @@ client.on("message", function(message){
 	 
 	});//Alpha_Codes
 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("493103430768787482");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("548464174230011904");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("493103430768787482");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DyOrwA.fvbdO_o3Xl2a-7R-0IDvqA0Joek');
