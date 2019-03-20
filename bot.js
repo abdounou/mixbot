443ffc1f37230b574ed91a9f5312bb776bf23830
abdounou <45997213+abdounou@users.noreply.github.com>
2019-03-20 16:42:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13591,304 +13591,129 @@ fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Backgro
 });
 });
 
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
- 
-var prefix = "!"
-client.on('message', async msg => {
-    if (msg.author.bot) return undefined;
-    if (!msg.content.startsWith(prefix)) return undefined;
-    const args = msg.content.split(' ');
-    const searchString = args.slice(1).join(' ');
-    const url = args[1] ? args[1] .replace(/<(.+)>/g, '$1') : '';
-    const serverQueue = queue.get(msg.guild.id);
-    let command = msg.content.toLowerCase().split(" ")[0];
-    command = command.slice(prefix.length)
-    if (command === `play`) {
-        const voiceChannel = msg.member.voiceChannel;
-        if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
-        const permissions = voiceChannel.permissionsFor(msg.client.user);
-        if (!permissions.has('CONNECT')) {
-            return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
+const { readFile, readFileSync } = require('fs-nextra');
+const cnvs = require("canvas");
+const { Canvas } = require("canvas-constructor")
+const { get } = require('snekfetch');
+const prefix = "!"
+
+let inv = JSON.parse(fs.readFileSync("./userD.json", "UTF8"))
+
+const invs = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+      if(g.members.get(client.user.id).hasPermission("MANAGE_GUILD"))
+    g.fetchInvites().then(guildInvites => {
+      invs[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invs[member.guild.id];
+    invs[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    inv[member.user.id+member.guild.id].inviter = invite.inviter.id;
+    fs.writeFile("./userD.json", JSON.stringify(inv), function(err) {
+            if(err) throw err;
+        });
+  });
+});
+
+client.on('message', async message => {
+    if(message.content.startsWith(prefix + "id")) {
+    let mem = message.mentions.members.first() || message.member;
+    let auth = message.mentions.users.first() || message.author;
+    const imageUrlRegex = /\?size=2048$/g;
+    const name = mem.displayName.length > 10 ? mem.displayName.substring(0, 11) + "..." : mem.displayName;
+
+    const { body: ava } = await get(auth.displayAvatarURL.replace(imageUrlRegex, "?size=128"));
+    
+    const img = await readFile("./id_1.png");
+    const millis = new Date().getTime() - auth.createdAt.getTime();
+    const now = new Date();
+    const createdAt = millis / 1000 / 60 / 60 / 24;
+    const millisj = new Date().getTime() - mem.joinedAt.getTime();
+    const nowj = new Date();
+    const joinedAt = millisj / 1000 / 60 / 60 / 24;
+    
+        if(!inv[mem.id+message.guild.id]) inv[mem.id+message.guild.id] = {
+            inviter: "Not stored in database",
+            totalSecs: 0
         }
-        if (!permissions.has('SPEAK')) {
-            return msg.channel.send('لا يتوآجد لدي صلاحية للتكلم بهذآ الروم');
+
+        fs.writeFile("./userD.json", JSON.stringify(inv), function(err) {
+            if(err) throw err;
+        });
+    // Invites
+    const guildInvites = await message.guild.fetchInvites();
+    let invites = 0;
+    guildInvites.forEach(i => {
+        if(i.inviter.id === auth.id) {
+            invites += i.uses;
         }
- 
-        if (!permissions.has('EMBED_LINKS')) {
-            return msg.channel.sendMessage("**يجب توآفر برمشن `EMBED LINKS`لدي **rl")
-            }
- 
-        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-            const playlist = await youtube.getPlaylist(url);
-            const videos = await playlist.getVideos();
-            for (const video of Object.values(videos)) {
-                const video2 = await youtube.getVideoByID(video.id);
-                await handleVideo(video2, msg, voiceChannel, true);
-            }
-            return msg.channel.send(` **${playlist.title}** تم الإضآفة إلى قأئمة التشغيل`);
-        } else {
-            try {
- 
-                var video = await youtube.getVideo(url);
- 
-            } catch (error) {
-                try {
-                                            var fast = {};
-                    var videos = await youtube.searchVideos(searchString, 10);
-                    let index = 0;
-                    const embed1 = new Discord.RichEmbed()
-                    .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
-${videos.map(video2 => `[**${++index}**] **${video2.title}**`).join('\n')}`)
-                    .setFooter(`${msg.guild.name}`)
-                    msg.channel.sendEmbed(embed1).then(message =>{
- 
-                        message.delete(15000)
- 
-                    });
-                    try {
-                        var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-                            maxMatches: 1,
-                            time: 20000,
-                            errors: ['time']
-                        })
- 
-                        }catch(err) {
-                        console.error(err);
-                        return msg.channel.send('لم يتم إختيآر مقطع صوتي');
-                        }
-                    const videoIndex = parseInt(response.first().content);
-                    var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
-                } catch (err) {
-                    console.error(err);
-                    return msg.channel.send(':x: لا يتوفر نتآئج بحث ');
-                }
-        }
- 
-            return handleVideo(video, msg, voiceChannel);
-        }
-    } else if (command === `skip`) {
-        if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-        if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لتجآوزه');
-        serverQueue.connection.dispatcher.end('تم تجآوز هذآ المقطع');
-        return undefined;
-    } else if (command === `stop`) {
-        if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-        if (!serverQueue) return msg.channel.send('لا يتوفر مقطع لإيقآفه');
-        serverQueue.songs = [];
-        serverQueue.connection.dispatcher.end('تم إيقآف هذآ المقطع');
-        return undefined;
-    } else if (command === `vol`) {
-        if (!msg.member.voiceChannel) return msg.channel.send('أنت لست بروم صوتي .');
-        if (!serverQueue) return msg.channel.send('لا يوجد شيء شغآل.');
-        if (!args[1]) return msg.channel.send(`:loud_sound: مستوى الصوت **${serverQueue.volume}**`);
-        serverQueue.volume = args[1];
-        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
-        return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
-    } else if (command === `np`) {
-        if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-        const embedNP = new Discord.RichEmbed()
-    .setDescription(`:notes: الان يتم تشغيل : **${serverQueue.songs[0].title}**`)
-        return msg.channel.sendEmbed(embedNP);
-    } else if (command === `replay`) {
-        if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-        const embedNP = new Discord.RichEmbed()
-    .setDescription(`سيتم اعاده تشغيل الفديو :**${serverQueue.songs[0].title}**`)
-    msg.channel.send({embed: embedNP})
-     return handleVideo(video, msg, msg.member.voiceChannel);
- 
-    } else if (command === `queue`) {
-        if (!serverQueue) return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-        let index = 0;
-        const embedqu = new Discord.RichEmbed()
-.setDescription(`**Songs Queue**
-${serverQueue.songs.map(song => `**${++index} -** ${song.title}`).join('\n')}
-**الان يتم تشغيل** ${serverQueue.songs[0].title}`)
-        return msg.channel.sendEmbed(embedqu);
-    } else if (command === `pause`) {
-        if (serverQueue && serverQueue.playing) {
-            serverQueue.playing = false;
-            serverQueue.connection.dispatcher.pause();
-            return msg.channel.send('تم إيقاف الموسيقى مؤقتا!');
-        }
-        return msg.channel.send('لا يوجد شيء حالي ف العمل.');
-    } else if (command === "resume") {
-        if (serverQueue && !serverQueue.playing) {
-            serverQueue.playing = true;
-            serverQueue.connection.dispatcher.resume();
-            return msg.channel.send('استأنفت الموسيقى بالنسبة لك !');
-        }
-        return msg.channel.send('لا يوجد شيء حالي في العمل.');
+    }) // اصلا البوت معاه انفايتات؟؟؟؟
+    // Inviter
+   
+    let inviter =client.users.get(inv[mem.id+message.guild.id].inviter);
+    
+        const { body: bot } = await get(message.guild.iconURL.replace(imageUrlRegex, "?size=128"));
+
+
+    let canvas = new Canvas(417, 181)
+    .addImage(img, 0, 0, 417, 181)
+    .addRoundImage(bot, 7, 1, 29, 29, 25)
+    .setShadowColor("rgba(22, 22, 22, 1)") // This is a nice colour for a shadow.
+    .setShadowOffsetY(3) // Drop the shadow by 5 pixels.
+  .setShadowBlur(03) // Blur the shadow by 10.
+  .save()
+    .addRoundImage(ava, 320, 55, 78, 78, 39)
+    .setTextAlign("center")
+    .setTextFont("8pt Cairo")
+    .setColor((mem.highestRole.hexColor === "#000000") ? "#ffffff" : mem.highestRole.hexColor)
+    .addText(name, 360, 162)
+    .setColor("#FFFFFF")
+    .addText(createdAt.toFixed(), 192, 77)
+    .addText((joinedAt.toFixed().length >= 3) ? joinedAt.toFixed() : joinedAt.toFixed() + "D", 257.5, 77)
+    .addText("voc", 195, 130)
+    .addText("tex", 258, 130)
+    .addText(`${inv[mem.id+message.guild.id].totalSecs} Sec`, 205, 163)
+    .addText((invites === 1 || invites === 0 ) ? invites + " Member" : invites + " Members", 120, 128)
+    .addText((inviter) ? inviter.username : "Not stored", 110, 77)
+    if(inviter) {
+            const { body: buffer } = await get(inviter.avatarURL.replace(imageUrlRegex, "?size=128"))
+
+        canvas.addRoundImage(buffer, 14, 59, 30, 30, 15)
+
     }
- 
-    return undefined;
-async function handleVideo(video, msg, voiceChannel, playlist = false) {
-    const serverQueue = queue.get(msg.guild.id);
-    const song = {
-        id: video.id,
-        title: Util.escapeMarkdown(video.title),
-        url: `https://www.youtube.com/watch?v=${video.id}`,
-        time:`${video.duration.hours}:${video.duration.minutes}:${video.duration.seconds}`,
-        eyad:`${video.thumbnails.high.url}`,
-        best:`${video.channel.title}`,
-        bees:`${video.raw.snippet.publishedAt}`,
-        shahd:`${video.raw.kind}`,
-        zg:`${video.raw.snippet.channelId}`,
-        views:`${video.raw.views}`,
-        like:`${video.raw.likeCount}`,
-        dislike:`${video.raw.dislikeCount}`,
-        hi:`${video.raw.id}`
-    };
-    if (!serverQueue) {
-        const queueConstruct = {
-            textChannel: msg.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-        queue.set(msg.guild.id, queueConstruct);
-        queueConstruct.songs.push(song);
-        try {
-            var connection = await voiceChannel.join();
-            queueConstruct.connection = connection;
-            play(msg.guild, queueConstruct.songs[0]);
-        } catch (error) {
-            console.error(`I could not join the voice channel: ${error}`);
-            queue.delete(msg.guild.id);
-            return msg.channel.send(`لا أستطيع دخول هذآ الروم ${error}`);
-        }
-    } else {
-        serverQueue.songs.push(song);
-        console.log(serverQueue.songs);
-        if (playlist) return undefined;
-        else return msg.channel.send(` **${song.title}** تم اضافه الاغنية الي القائمة!`);
+    
+    //.addText("Joined at: ", 120, 100)
+    message.channel.send({ file: canvas.toBuffer()})
     }
-    return undefined;
+})
+
+client.on('voiceStateUpdate', (o, n) => {
+    if(n.user.bot) return;
+    if(!inv[n.user.id+n.guild.id].totalSecs) inv[n.user.id+n.guild.id].totalSecs = 0;
+    int(o, n);
+    
+})
+function int(o, n) {
+    var inter = setInterval(() => {
+        if(!n.voiceChannel) return clearInterval(inter);
+        if(n.voiceChannel === n.guild.afkChannel) return clearInterval(inter)
+        inv[n.user.id+n.guild.id].totalSecs++;
+         fs.writeFile("./userD.json", JSON.stringify(inv), function(err) {
+            if(err) throw err;
+        });
+    }, 1000)
 }
- 
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
- 
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-    console.log(serverQueue.songs);
-    const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-        .on('end', reason => {
-            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-            else console.log(reason);
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on('error', error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-        fetchVideoInfo(`${song.hi}`, function (err, fuck) {
-  if (err) throw new Error(err);
-  console.log(fuck);
-      const yyyy = {}
-  if(!yyyy[msg.guild.id]) yyyy[msg.guild.id] = {
-    like: `${fuck.likeCount}`,
-    dislike: `${fuck.dislikeCount}`
-  }
-    serverQueue.textChannel.send({embed : new Discord.RichEmbed()
-  .setTitle(`**${fuck.title}**`)
-  .setURL(fuck.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${fuck.datePublished}`, true)
-  .addField('Views :' , `${fuck.views}`, true)
-  .addField('Like👍 :' , `${fuck.likeCount}`, true)
-  .addField('dislike👎 :' , `${fuck.dislikeCount}`, true)
-  .addField('comments :' , `${fuck.commentCount}`, true)
-    .setImage(`${song.eyad}`)
-    .setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-    .setColor('#ff0000')
-    .setTimestamp()
-    }).then(love => {
-        love.react('👍').then(r=>{
-        love.react('👎').then(r =>{
-        love.react('🙌').then(r=> {
-    let likee = (reaction, user) => reaction.emoji.name === '👍' && user.id === msg.author.id;
-    let dislikee = (reaction, user) => reaction.emoji.name === '👎' && user.id === msg.author.id;
-    let cnn = (reaction, user) => reaction.emoji.name === '🙌' && user.id === msg.author.id;
- 
-    let ll = love.createReactionCollector(likee , {max:5});
-    let dd = love.createReactionCollector(dislikee , {max:5});
-    let cn = love.createReactionCollector(cnn , {max:5});
- 
-            ll.on("collect", r => {
-              yyyy[msg.guild.id].like++;
-    love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${fuck.title}**`)
-  .setURL(fuck.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${fuck.datePublished}`, true)
-  .addField('Views :' , `${fuck.views}`, true)
-  .addField('Like👍 :' , `${yyyy[msg.guild.id].like}`, true)
-  .addField('dislike👎 :' , `${fuck.dislikeCount}`, true)
-  .addField('comments :' , `${fuck.commentCount}`, true)
-    .setImage(`${song.eyad}`)
-    .setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-    .setColor('#ff0000')
-    .setTimestamp()
-});
-    })
- 
-    dd.on("collect", r => {
-      yyyy[msg.guild.id].dislike++;
-    love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${fuck.title}**`)
-  .setURL(fuck.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${fuck.datePublished}`, true)
-  .addField('Views :' , `${fuck.views}`, true)
-  .addField('Like👍 :' , `${fuck.likeCount}`, true)
-  .addField('dislike👎 :' , `${yyyy[msg.guild.id].dislike}`, true)
-  .addField('comments :' , `${fuck.commentCount}`, true)
-    .setImage(`${song.eyad}`)
-    .setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-    .setColor('#ff0000')
-    .setTimestamp()
-});
-})
-    cn.on("collect", r => {
-    love.edit({embed : new Discord.RichEmbed()
-  .setTitle(`**${fuck.title}**`)
-  .setURL(fuck.url)
-  .addField('Time The Video :' , `${song.time}`, true)
-  .addField('Channel Name :' , `${song.best}`, true)
-  .addField('Channel ID :' , `${song.zg}`, true)
-  .addField('Video Created at :' , `${fuck.datePublished}`, true)
-  .addField('Views :' , `${fuck.views}`, true)
-  .addField('Like👍 :' , `${fuck.likeCount}`, true)
-  .addField('dislike👎 :' , `${fuck.dislikeCount}`, true)
-  .addField('comments :' , `${fuck.commentCount}`, true)
-    .setImage(`${song.eyad}`)
-    .setThumbnail('http://cdn.akhbaar24.com/430e061a-f89a-43c7-86d9-82fae5f7c495.jpg')
-    .setColor('#ff0000')
-    .setTimestamp()
-});
-})
-})
-})
-})
-})
-})
-}
-});
 
 client.login('NTI5NjA5NTM1NTQ4MTYyMDU5.DyOrwA.fvbdO_o3Xl2a-7R-0IDvqA0Joek');
